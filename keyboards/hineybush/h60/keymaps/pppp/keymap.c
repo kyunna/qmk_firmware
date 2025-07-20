472ed52afc0518f55d5cd84ef119b1966d9f354b
olly <olly@pppp.page>
2025-07-20 18:44:35 +0900

    Update common keymap and functions

---
+++ keyboards/hineybush/h60/keymaps/pppp/keymap.c
@@ -0,0 +1,33 @@
+#include QMK_KEYBOARD_H
+#include "keymap.h"
+#include "keymap_60_hhkb.h"
+
+/* Initial RGB configuration: number of LEDs and startup mode. */
+#define LED_COUNT 16
+#define INITIAL_RGB_MODE RGBLIGHT_MODE_STATIC_LIGHT
+
+/* Default backplate color (lime green) */
+#define RGB_R 50
+#define RGB_G 205
+#define RGB_B 50
+
+/* Apply and save RGB color to all LEDs. */
+void set_rgb(void) {
+    for (int i = 0; i < LED_COUNT; i++) {
+        rgblight_setrgb_at(RGB_R, RGB_G, RGB_B, i);
+
+        last_rgb_state[i][0] = RGB_R;
+        last_rgb_state[i][1] = RGB_G;
+        last_rgb_state[i][2] = RGB_B;
+    }
+
+    last_rgb_mode = INITIAL_RGB_MODE;
+    led_count = LED_COUNT;
+}
+
+/* Called once after keyboard initializes. */
+void keyboard_post_init_user(void) {
+    rgblight_enable_noeeprom();
+    rgblight_mode_noeeprom(INITIAL_RGB_MODE);
+    set_rgb();
+}
---
+++ users/pppp/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+/* QMK behavior configuration */
+#define TAPPING_TERM    160 // Tap-hold decision time (ms)
+#define COMBO_TERM      30  // Time window to press combos (ms)
+#define PERMISSIVE_HOLD     // Allow hold if other key is pressed during tap
